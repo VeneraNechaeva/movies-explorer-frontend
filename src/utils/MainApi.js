a0433704d8c721ce4fa89598f5d470618b6dbe96
Venera Nechaeva <venera_nechaeva@mail.ru>
2023-10-12 10:23:32 +0300

    feat: добавить baseUrl

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -87,73 +87,11 @@ class MainApi {
       headers: this._headers
     })
   }
-
-  // // Получить начальные карточки с сервера
-  // getInitialCards() {
-  //   return this._sendRequest(`${this._baseUrl}/cards`, {
-  //     method: 'GET',
-  //     headers: this._headers
-  //   })
-  // }
-  // // Добавить на сервер новую карточку
-  // addNewCard(name, link) {
-  //   return this._sendRequest(`${this._baseUrl}/cards`, {
-  //     method: 'POST',
-  //     headers: { ...this._headers, ...{ 'Content-Type': 'application/json' } },
-  //     body: JSON.stringify({
-  //       name: name,
-  //       link: link
-  //     })
-  //   })
-  // }
-
-  // // Удаление карточки
-  // deletCard(cardId) {
-  //   return this._sendRequest(`${this._baseUrl}/cards/${cardId}`, {
-  //     method: 'DELETE',
-  //     headers: this._headers
-  //   })
-  // }
-
-  // // Отвечает за статус лайка
-  // changeLikeCardStatus(cardId, isLiked) {
-  //   if (isLiked) { return this.setLike(cardId); }
-  //   else { return this.deletLike(cardId); }
-  // }
-
-  // // Постановка и снятие лайка
-  // // Постановка лайка
-  // setLike(cardId) {
-  //   return this._sendRequest(`${this._baseUrl}/cards/${cardId}/likes`, {
-  //     method: 'PUT',
-  //     headers: this._headers
-  //   })
-  // }
-  // // Снятие лайка
-  // deletLike(cardId) {
-  //   return this._sendRequest(`${this._baseUrl}/cards/${cardId}/likes`, {
-  //     method: 'DELETE',
-  //     headers: this._headers
-  //   })
-  // }
-
-  // // Обновление аватара пользователя
-  // changeAvatar(avatar) {
-  //   return this._sendRequest(`${this._baseUrl}/users/me/avatar`, {
-  //     method: 'PATCH',
-  //     headers: { ...this._headers, ...{ 'Content-Type': 'application/json' } },
-  //     body: JSON.stringify({
-  //       avatar: avatar,
-  //     })
-  //   })
-  // }
-
-
 }
 
 // Создание экземпляров класса Api
 export const api = new MainApi({
-  baseUrl: 'http://localhost:3001',
+  baseUrl: 'https://api.movies.nechaeva.nomoredomainsicu.ru',
   headers: {
     'Content-Type': 'application/json'
   }
